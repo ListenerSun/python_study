43aa40a876a3ac9a9bd0526ccff63f89d7042e9d
ListenerSun <ListenerSun@163.com>
2019-12-26 23:37:17 +0800

    add function 函数 study

---
+++ SayHelloFunction.py
@@ -0,0 +1,10 @@
+#   say_hello() 函数
+
+
+def say_hello():
+    """打招呼函数
+    函数只有调用的时候才执行
+    """
+    print("Hello ListenerSun")
+    print("Hello ListenerSun")
+    print("Hello ListenerSun")
---
+++ TestSayHelloFunction.py
@@ -0,0 +1,4 @@
+import SayHelloFunction
+
+SayHelloFunction.say_hello()
+print("调用了 say_hello 函数")
